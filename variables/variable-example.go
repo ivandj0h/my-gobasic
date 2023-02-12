82fd4d6eeca6e9564c10340485e34460a68c6324
ivandjoh <ivandi.djoh@gmail.com>
2023-02-13 01:29:05 +0800

    Variable examples updated!

--- variables/variable-example.go
+++ variables/variable-example.go
@@ -1,6 +1,9 @@
 package variables
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 // ExampleOfVariable /**
 //   - This function is used to show how to use variable in Go
@@ -34,4 +37,47 @@ func ExampleOfVariableWithMultipleParametersAndReturnValue(
 	address string,
 ) (string, int, string) {
 	return name, age, address
+}
+
+// PrintsAllVariables /**
+// This is another way to create variable in Go
+var (
+	name1    string
+	age1     int
+	address1 string
+)
+
+// This is another way to create variable in Go
+var name2, age2, address2 string
+
+// This is another way to create variable in Go
+var name3 string
+var age3 int
+var address3 string
+
+// This is another way to create variable in Go
+var (
+	name4    = "ivan"
+	age4     = 43
+	address4 = "Jakarta"
+)
+
+func PrintsAllVariables() {
+
+	// Add value to the variable 1
+	name1, age1, address1 = "ivan", 11, "Jakarta"
+
+	// Add value to the variable 2
+	name2, age2, address2 = "ivan", strconv.Itoa(22), "Jakarta"
+
+	// Add value to the variable 3
+	name3, age3, address3 = "ivan", 33, "Jakarta"
+
+	// Add value to the variable 4
+	name4, age4, address4 = "ivan", 44, "Jakarta"
+
+	fmt.Println(name1, age1, address1)
+	fmt.Println(name2, age2, address2)
+	fmt.Println(name3, age3, address3)
+	fmt.Println(name4, age4, address4)
 }
