c7e06d7ed7a53474c11e13724fbe45cfb1cc005c
ivandjoh <ivandi.djoh@gmail.com>
2023-02-13 19:43:56 +0800

    Array Examples

--- arrays/array-example.go
+++ arrays/array-example.go
@@ -3,5 +3,20 @@ package arrays
 import "fmt"
 
 func ExampleArray() {
-	fmt.Println("Array Example")
+
+	// Shorthand declaration of array
+	arr := [4]string{"one", "two", "three", "four"}
+	fmt.Println("This is Example of the Array :")
+	for i := 0; i < len(arr); i++ {
+		fmt.Println(arr[i])
+	}
+}
+
+func GetTheLengthOfArray() {
+
+	// Shorthand declaration of array
+	arrOne := [4]string{"one", "two", "three", "four"}
+	arrTwo := [...]string{"one", "two", "three", "four", "five", "six"}
+	fmt.Println("The length of the array One is :", len(arrOne))
+	fmt.Println("The length of the array Two is :", len(arrTwo))
 }
